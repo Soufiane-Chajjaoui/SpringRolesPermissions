d4684abd867a4eeb0b988098e0c18a4e9f15c825
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-02-23 13:41:30 +0100

    first commit

---
+++ src/test/java/est/projet/springrolespermission/SpringRolesPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package est.projet.springrolespermission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRolesPermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
