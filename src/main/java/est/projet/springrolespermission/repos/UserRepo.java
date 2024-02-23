0754da749d3de05980cc365ac7de492903f93389
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-02-23 22:48:51 +0100

    Repositories Token and User

---
+++ src/main/java/est/projet/springrolespermission/repos/UserRepo.java
@@ -0,0 +1,12 @@
+package est.projet.springrolespermission.repos;
+
+import est.projet.springrolespermission.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User , Long> {
+    Optional<User> findByEmail(String username);
+}
