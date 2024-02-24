b78dbf38fff4033be4c85101352c704d13f591fa
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-02-24 21:08:21 +0100

    request and response Objects for WebService

---
+++ src/main/java/est/projet/springroles/auth/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package est.projet.springroles.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+}
